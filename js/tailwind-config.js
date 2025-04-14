8c66fa345374c9916c9f9b39386fbd38330969a5
Charly Cardeño <charly@elemento43.com>
2025-04-14 10:05:14 -0500

    Initial commit of website files

---
+++ js/tailwind-config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'apple-gray-100': '#f5f5f7',
+        'apple-gray-200': '#e8e8ed',
+        'apple-gray-300': '#d2d2d7',
+        'apple-gray-400': '#a1a1a6',
+        'apple-gray-500': '#86868b',
+        'apple-gray-600': '#6e6e73',
+        'apple-gray-700': '#1d1d1f',
+        'apple-gray-800': '#121214',
+        'apple-gray-900': '#000000'
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      }
+    }
+  }
+}
